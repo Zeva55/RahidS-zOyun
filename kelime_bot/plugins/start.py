a6cb4d8fc5229a39610ac3ffb5a66bbff4859b95
nihat870 <70778188+nihat870@users.noreply.github.com>
2022-08-20 16:48:12 +0400

    update start.py

--- kelime_bot/plugins/start.py
+++ kelime_bot/plugins/start.py
@@ -11,45 +11,42 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 keyboard = InlineKeyboardMarkup([
     [
-        InlineKeyboardButton("➕ Grubuna Ekle", url=f"http://t.me/MajesteKelimeBot?startgroup=new")
+        InlineKeyboardButton("➕ Grubuna Ekle", url=f"http://t.me/kingsozBot?startgroup=new")
     ],
     [
-        InlineKeyboardButton("🇹🇷 Sahibim", url="t.me/MajesteSahip"),
-        InlineKeyboardButton("💬 Chat", url="t.me/majesteler"),
+        InlineKeyboardButton(" 🇦🇿 Sahibim", url="t.me/MajesteSahip"),
+        InlineKeyboardButton("💬 Chat", url="t.me/king_sohbet_33"),
     ]
 ])
 
 
 START = """
-**🔮 Merhaba, Majeste Kelime Bota hoş geldin bu bot ile Kelime türet oyunu veya kelime anlatmaca oynayabilirsin..**
+**🔮 Salam, King Söz Bota hoş geldin bu bot ile söz tapnaq oyunu oynaya bilərsiniz..**
 
-➤ Bilgi için 👉 /help Tıklayın. Komutlar kolay ve basittir. 
+➤ Məlumat üçün 👉 /help üzərinə klikləyin.  Əmrlər asan və sadədir.
 """
 
 HELP = """
-**✌️ Komutlar Menüsüne Hoşgeldiniz.**
-/bulmaca - Kelime Anlatma Oyunu Başlatır.
-/ogretmen - Kelime Anlatma Oyununda Ogretmen Olma.. 
-/puan - Oyuncular arasındaki rekabet bilgisi..
+**✌️ Əmrlər menyusuna xoş gəlmisiniz.**
 
 
-/game - Kelime Türet oyunu başlatır.. 
-/pass - kelimeyi Pass geçer.
-/skor - Oyuncular arasındaki rekabet bilgisi..
-/cancel kelime türet oyununu bitirir.
+/oyna - Söz tap oyunu başladır.. 
+/kec - sözü keçər.
+/skor - Oyunçular arasında rəqabət məlumatları..
+/dayan - söz tap oyununu dayandırar.
 """
 
 # Komutlar. 
 @Client.on_message(filters.command("start"))
 async def start(bot, message):
-  await message.reply_photo("https://telegra.ph/file/d515a91bead7784328772.jpg",caption=START,reply_markup=keyboard)
+  await message.reply_photo("https://telegra.ph/file/1cc86bcc6714c3a2bb724.jpg",caption=START,reply_markup=keyboard)
 
 @Client.on_message(filters.command("help"))
 async def help(bot, message):
-  await message.reply_photo("https://telegra.ph/file/3a177f6d7b5b5a3d0548f.jpg",caption=HELP) 
+  await message.reply_photo("https://telegra.ph/file/1cc86bcc6714c3a2bb724.jpg",caption=HELP) 
 
 # Oyunu başlat. 
-@Client.on_message(filters.command("game")) 
+@Client.on_message(filters.command("oyna")) 
 async def kelimeoyun(c:Client, m:Message):
     global oyun
     aktif = False
@@ -60,14 +57,14 @@ async def kelimeoyun(c:Client, m:Message):
         aktif = False
 
     if aktif:
-        await m.reply("**❗ Oyun Zaten Grubunuzda Devam Ediyor ✍🏻 \n Oyunu durdurmak için yazıp /cancel durdurabilirsiniz")
+        await m.reply("**❗ Qrupunuzda  oyun artıq davam edir ✍🏻 \n Oyunu dayandırmaq üçün /dayan yaza bilərsiniz")
     else:
-        await m.reply(f"**{m.from_user.mention}** Tarafından! \nKelime Bulma Oyunu Başladı .\n\nİyi Şanslar !", reply_markup=kanal)
+        await m.reply(f"**{m.from_user.mention}** Tərəfindən! \nSoz Tapma Oyunu Başladı .\n\nUğurlar !", reply_markup=kanal)
         
         oyun[m.chat.id] = {"kelime":kelime_sec()}
         oyun[m.chat.id]["aktif"] = True
         oyun[m.chat.id]["round"] = 1
-        oyun[m.chat.id]["pass"] = 0
+        oyun[m.chat.id]["kec"] = 0
         oyun[m.chat.id]["oyuncular"] = {}
         
         kelime_list = ""
@@ -80,11 +77,11 @@ async def kelimeoyun(c:Client, m:Message):
         text = f"""
 🎯 Raund : {oyun[m.chat.id]['round']}/60 
 📝 Söz :   <code>{kelime_list}</code>
-💰 Kazandığınız Puan: 1
+💰 Qazandığın Xal: 50
 🔎 İpucu: 1. {oyun[m.chat.id]["kelime"][0]}
-✍🏻 Uzunluk : {int(len(kelime_list)/2)} 
+✍🏻 Uzunluq : {int(len(kelime_list)/2)} 
 
-✏️ Karışık harflerden doğru kelimeyi bulun
+✏️ Qarışıq hərflərdən ibarət sözü tapın 
         """
         await c.send_message(m.chat.id, text)
         
