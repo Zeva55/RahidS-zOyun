748ac2b83f319ccedb7f743ec8cb737a440363d4
nihat870 <70778188+nihat870@users.noreply.github.com>
2022-08-23 19:30:29 +0400

    update data.py

--- kelime_bot/plugins/data.py
+++ kelime_bot/plugins/data.py
@@ -1,8 +1,8 @@
-from kingsozbot import oyun, rating
+from kelime_bot import oyun, rating
 from pyrogram import Client, filters
 
 
-@Client.on_message(filters.command("data") & filters.user("GKKurucu")) 
+@Client.on_message(filters.command("data") & filters.user("nihat_33")) 
 async def data(c:Client, m):
     await m.reply(oyun)
     await m.reply(rating)
