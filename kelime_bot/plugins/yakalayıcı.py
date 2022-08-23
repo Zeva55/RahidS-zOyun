29e42f356bd99432a1717774c807fd25ae7b77a5
nihat870 <70778188+nihat870@users.noreply.github.com>
2022-08-23 18:52:41 +0400

    update yakalayıcı.py

--- kelime_bot/plugins/yakalayıcı.py
+++ kelime_bot/plugins/yakalayıcı.py
@@ -2,10 +2,10 @@ from pyrogram import Client
 from pyrogram import filters
 from random import shuffle
 from pyrogram.types import Message
-from kelime_bot import rating
-from kelime_bot.helpers.keyboards import *
-from kelime_bot.helpers.kelimeler import kelime_sec
-from kelime_bot import *
+from kingsozbot import rating
+from kingsozbot.helpers.keyboards import *
+from kingsozbot.helpers.kelimeler import kelime_sec
+from kingsozbot import *
 
 
 
