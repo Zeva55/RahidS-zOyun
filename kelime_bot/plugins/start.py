6cf637ee9c6314f0f65aad636a708cef0adb0559
Rahid2003 <104177696+Rahid2003@users.noreply.github.com>
2022-10-03 11:32:46 +0400

    update start.py

--- kelime_bot/plugins/start.py
+++ kelime_bot/plugins/start.py
@@ -84,8 +84,6 @@ async def kelimeoyun(c:Client, m:Message):
 📄 Uzunluq: {int(len(kelime_list)/2)}
 
 💡 İlk hərf: {oyun[m.chat.id]["kelime"][0]}
-
-@Cenublar qrupumuza dəvətlisiniz❤️
         """
         await c.send_message(m.chat.id, text)
         
