d0579d8742f3b168d61d57b3c599af8d27043233
Zeva55 <113102760+Zeva55@users.noreply.github.com>
2022-11-28 11:53:29 +0400

    Update and rename kelime_bot/helpers/keyboards.py to kelime_bot/köməkçilər/keyboards.py

--- kelime_bot/köməkçilər/keyboards.py
+++ kelime_bot/köməkçilər/keyboards.py
@@ -2,7 +2,7 @@ from pyrogram.types import InlineKeyboardButton
 from pyrogram.types import InlineKeyboardMarkup
 
 kanal = InlineKeyboardMarkup([
-    [InlineKeyboardButton("🔮 Əyləncə Qrupu 🔮" , url= "t.me/cenublar")]
+    [ InlineKeyboardButton ( "🔮 Əyləncə Qrupu 🔮" , url           =            "t.me/BigTelegramTribe")
 ])
 
 
